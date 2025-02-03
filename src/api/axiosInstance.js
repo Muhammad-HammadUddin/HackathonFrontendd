f21e3b0b7a2e7924d7c247d1b7860335875247f7
Muhammad Hammad Uddin <hammaduddin083@gmail.com>
2025-02-03 21:39:01 +0500

    baseURL updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://benificiaryappbackend-production.up.railway.app/', // Direct backend URL
+    baseURL: 'https://benificiaryappbackend-production-6862.up.railway.app/', // Direct backend URL
     withCredentials: true,
 });
